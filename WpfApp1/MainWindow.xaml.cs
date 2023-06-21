71e30e0d254f4771ee9d01bda288e5caac4d468e
keencho <seyoung050412@gmail.com>
2023-06-21 13:08:31 +0900

    이벤트 로그(?) 쌓기 -> arraylist로

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
@@ -30,41 +34,51 @@ namespace WpfApp1
             }
         }
     }
-    
+
     public partial class MainWindow : Window
     {
-        private readonly KeyboardClickEventModel _keyboardClickEventModel = new KeyboardClickEventModel();
+        private readonly KeyboardClickEventModel _keyboardClickEventModel = new();
+        private List<string> logs = new();
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = _keyboardClickEventModel;
+            Start();
+        }
+
+        private void Start()
+        {
+            Debug.WriteLine("====================키보드 바인딩 이벤트 시작");
+            _keyboardClickEventModel.IsListening = true;
+            PreviewKeyDown += MainWindow_PreviewKeyDown;
+        }
+
+        private void Stop()
+        {
+            Debug.WriteLine("====================키보드 바인딩 이벤트 종료");
+            _keyboardClickEventModel.IsListening = false;
+            PreviewKeyDown -= MainWindow_PreviewKeyDown;
         }
 
         private void KeyboardClickEventHandler(object sender, RoutedEventArgs e)
         {
             if (_keyboardClickEventModel.IsListening)
-            {
-                Debug.WriteLine("====================키보드 바인딩 이벤트 종료");
-                _keyboardClickEventModel.IsListening = false;
-                PreviewKeyDown -= MainWindow_PreviewKeyDown;
-            }
+                Stop();
             else
-            {
-                Debug.WriteLine("====================키보드 바인딩 이벤트 시작");
-                _keyboardClickEventModel.IsListening = true;
-                PreviewKeyDown += MainWindow_PreviewKeyDown;
-            }
+                Start();
         }
 
         private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (_keyboardClickEventModel.IsListening)
             {
-                Debug.WriteLine(e.Key);
+                logs.Add($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {e.Key.ToString()} is pressed!");
+                TextBlock.Text = string.Join(Environment.NewLine, logs);
                 // if (e.Key is Key.Q)
                 // {
-                //     Debug.WriteLine("Hello, World!");   
+                //     Debug.WriteLine("Hello, World!");
+                //     MessageBox.Show("Button Clicked!", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
                 // }
             }
         }
